f71d692a684644653cfcba6ad4a24931e29a05dd
Yoku <icarolsl@gmail.com>
2026-02-16 10:02:59 -0300

    Remove unnecessary comments from build.gradle.kts and LoginController.java

--- Back-End/app/src/main/java/com/voyago/controller/LoginController.java
+++ Back-End/app/src/main/java/com/voyago/controller/LoginController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.net.InetAddress;
+// import java.net.InetAddress;
 import java.util.Optional;
 
 @RestController
